0102adc4662340cf15dc41da8b2d0b54d3e31231
Sh1617 <22f3001953@ds.study.iitm.ac.in>
2026-05-19 09:53:03 +0530

    Frontend complete

--- backend/embedder.py
+++ backend/embedder.py
@@ -1,10 +1,13 @@
-from sentence_transformers import SentenceTransformer
+#from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+#model = SentenceTransformer("all-MiniLM-L6-v2")
 
+
+#def generate_embedding(text):
+
+    #embedding = model.encode(text)
+
+    #return embedding.tolist()
 
 def generate_embedding(text):
-
-    embedding = model.encode(text)
-
-    return embedding.tolist()
+    return [0.0] * 384
